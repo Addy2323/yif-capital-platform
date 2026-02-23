f094b0dd7477eaf5e618171c558503af7787c0fe
Addy2323 <myambaado@gmail.com>
2026-02-23 08:22:11 -0800

    new

--- lib/fund-utils.ts
+++ lib/fund-utils.ts
@@ -2,10 +2,11 @@
 // This is needed because some funds use different slugs in URLs vs their DB fundId
 export const FUND_ID_MAP: Record<string, string> = {
     "zansec": "zansec-bond",
-    "utt-amis": "utt-umoja",
+    "utt-amis": "utt-amis",
     "whi": "whi-income",
     "vertex": "vertex-bond",
     "sanlam-pesa": "sanlam-pesa",
+    "itrust": "itrust",
 }
 
 export function resolveFundId(urlId: string): string {
